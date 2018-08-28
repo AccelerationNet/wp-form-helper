1f1c1fe4497d8586a5946e16b93d8a0641297258
Russ Tyndall <russ@acceleration.net>
2018-08-28 17:15:18 -0400

    Added some more functionality to minimal form helper functions

--- form-helper.js
+++ form-helper.js
@@ -1,16 +1,65 @@
 if(typeof(WPFH) == 'undefined') WPFH = {};
 console.log('Loading wpfh');
+
+WPFH.debounce = function(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      if (immediate && !timeout) func.apply(context, args);
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+    };
+};
+
+WPFH.throttle = function throttle (func, wait) {
+    var context, args, timeout, throttling, more, result;
+    var whenDone = _.debounce(function(){ more = throttling = false; }, wait);
+    return function() {
+      context = this; args = arguments;
+      var later = function() {
+        timeout = null;
+        if (more) func.apply(context, args);
+        whenDone();
+      };
+      if (!timeout) timeout = setTimeout(later, wait);
+      if (throttling) {
+        more = true;
+      } else {
+        result = func.apply(context, args);
+      }
+      whenDone();
+      throttling = true;
+      return result;
+    };
+};
+
+WPFH.toCurrency = function toCurrency(it){
+  return Number(it).toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+}
+
 WPFH.bind  = function bind(el, o){
   jQuery.each(o, function(k, v){
     // console.log('Binding ', k, v, o, el);
-    var inps = jQuery('[name='+k+']', el).each(function(i, inp){
+    if(k.startsWith('$')) return true;
+    var inps = jQuery('[name='+k+'],.'+k, el).each(function(i, inp){
       inp = jQuery(inp);
       if (inp.is('[type=radio],[type=checkbox]')){
         inp.prop('checked', inp.val() == v);
       }
-      else if (inp.is(':input')) inp.val(v);
+      else if (inp.is(':input')){
+        if(inp.hasClass('currency')) inp.val(Number(v).toFixed(2));
+        else inp.val(v);
+      }
       else if (inp.is('a') && k == "email") inp.attr('href', 'mailto:'+v);
-      else inp.text(v);
+      else if (inp.is('span')){
+        if(inp.hasClass('currency'))inp.text(WPFH.toCurrency(v));
+        else if (inp.hasClass('as-html')) inp.html(v);
+        else inp.text(v);
+      }
     });
   });
 };
@@ -43,3 +92,24 @@ WPFH.serializeForm = function(el){
   WPFH.setObjectVals(el, rtn);
   return rtn;
 };
+
+WPFH._plus = new RegExp('\\+','g');
+WPFH.parseQuery = function(query) {
+  if(!query) query = window.location.search.substring(1);
+  var obj = {};
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+    var k = decodeURIComponent(pair[0]).replace(WPFH._plus, " ");
+    var v = decodeURIComponent(pair[1]).replace(WPFH._plus, " ");
+    if(obj[k]){
+      if (angular.isArray(obj[k])) obj[k].push(v);
+      else obj[k]= [obj[k],v];
+    }
+    else obj[k]=v;
+  }
+  return obj;
+};
+
+WPFH.query = WPFH.parseQuery();
+WPFH.hashQuery = WPFH.parseQuery(window.location.hash.substring(1));
